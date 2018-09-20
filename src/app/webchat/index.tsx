e5dd25d94e34b4ce8bfb35efe29e71157f941e8f
Douglas Araujo <douglas.araujo@office.vulcanet.com.br>
2018-09-20 16:03:27 -0300

    Create Redux store in the WebChat application

--- src/app/webchat/index.tsx
+++ src/app/webchat/index.tsx
@@ -3,6 +3,16 @@
  */
 import {Component} from 'react';
 import React from 'react';
+import {Provider} from 'react-redux';
+import {Middleware} from 'redux';
+import {Reducer} from 'redux';
+import {Store as IStore} from 'redux';
+import {StoreEnhancer} from 'redux';
+import reducers from 'src/state/ducks';
+import Persistor from 'src/state/enhancers/persistor';
+import Storage from 'src/state/enhancers/persistor/storage';
+import middlewares from 'src/state/middlewares';
+import Store from 'src/store';
 
 
 /**
@@ -10,6 +20,47 @@ import React from 'react';
  */
 class WebChat extends Component
 {
+    // define fields
+    enhancer: StoreEnhancer;
+    middlewares: Middleware[];
+    persistor: Persistor;
+    reducer: Reducer;
+    store: IStore;
+
+
+    /**
+     * I initialize myself.
+     *
+     * :param props: component properties
+     *
+     * :returns: nothing
+     */
+    constructor (props: any)
+    {
+        // call the superclass constructor
+        super(props);
+
+        // set middlewares
+        this.middlewares = middlewares;
+
+        // set persistor
+        this.persistor = new Persistor('converja-webchat',
+                                       Store.combineReducers(reducers),
+                                       Storage);
+
+        // set store enhancer
+        this.enhancer = this.persistor.persist();
+
+        // set reducer
+        this.reducer = this.persistor.reducer;
+
+        // create global redux store
+        this.store = Store.create(this.reducer,
+                                  this.middlewares,
+                                  this.enhancer);
+    }
+
+
     /**
      * I render myself.
      *
@@ -18,7 +69,9 @@ class WebChat extends Component
     render (): React.ReactNode
     {
         return (
-            <div />
+            <Provider store={this.store}>
+                <div />
+            </Provider>
         );
     }
 }
