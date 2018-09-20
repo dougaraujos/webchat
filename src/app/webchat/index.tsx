0b79d8403fc09c0a3399a4c0f347de04036fe814
Douglas Araujo <douglas.araujo@office.vulcanet.com.br>
2018-09-20 12:06:28 -0300

    Add WebChat application skeleton

---
+++ src/app/webchat/index.tsx
@@ -0,0 +1,30 @@
+/**
+ * IMPORTS
+ */
+import {Component} from 'react';
+import React from 'react';
+
+
+/**
+ * I am a web chat application.
+ */
+class WebChat extends Component
+{
+    /**
+     * I render myself.
+     *
+     * :returns: web chat element
+     */
+    render (): React.ReactNode
+    {
+        return (
+            <div />
+        );
+    }
+}
+
+
+/**
+ * EXPORTS
+ */
+export default WebChat;
