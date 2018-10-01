fea6ba68c9aa89500f52d95ad76a31208ab6a448
Douglas Araujo <douglas.araujo@office.vulcanet.com.br>
2018-10-01 16:39:37 -0300

    Add icon types

---
+++ src/components/icon/icontypes.tsx
@@ -0,0 +1,18 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+import {IIconTypes} from 'src/components/icon/icontypes.d';
+import {types} from 'src/components/icon/icontypes.d';
+
+
+/**
+ * Icon types.
+ */
+const IconTypes: IIconTypes = {};
+
+
+/**
+ * EXPORTS
+ */
+export default IconTypes;
