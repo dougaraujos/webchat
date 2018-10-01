7c56ba91bfd39554ec7e17c17407a739701588ad
Douglas Araujo <douglas.araujo@office.vulcanet.com.br>
2018-10-01 16:41:27 -0300

    Add icon component definitions

---
+++ src/components/icon/index.d.ts
@@ -0,0 +1,16 @@
+/**
+ * IMPORTS
+ */
+import {types} from 'src/components/icon/icontypes.d';
+
+
+/**
+ * Icon properties interface.
+ */
+export interface IProps
+{
+    className?: string;
+    icon: any;
+    size?: any;
+    type?: types;
+}
